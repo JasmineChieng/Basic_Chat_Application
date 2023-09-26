e83975789d5ca472bf625afc1d319a392c2361e0
Jasmine Chieng <700036618@student.curtin.edu.my>
2023-09-26 12:39:28 +0800

    Connected the BusinessTierServer to the DatabaseServer.

--- ChatApplication/BusinessTierServer/BusinessInterfaceImpl.cs
+++ ChatApplication/BusinessTierServer/BusinessInterfaceImpl.cs
@@ -17,15 +17,15 @@ namespace BusinessTierServer
         private List<User> users = new List<User>();
         private List<ChatGroup> chatGroups = new List<ChatGroup>();
 
-         public BusinessInterfaceImpl()
+/*         public BusinessInterfaceImpl()
         {
             users = LoadUserData();
             chatGroups = LoadChatGroups();
-        }
+        }*/
 
         private ServerInterface foob;
 
-/*        public BusinessInterfaceImpl()
+        public BusinessInterfaceImpl()
         {
 
             ChannelFactory<ServerInterface> foobFactory;
@@ -36,23 +36,23 @@ namespace BusinessTierServer
             foob = foobFactory.CreateChannel();
 
 
-        }*/
+        }
 
         public bool RegisterUser(User user)
         {
             // Check if the username is already taken
-            if (users.Any(u => u.Username == user.Username))
-            {
-                return false; // Username already exists
-            }
+            // if (users.Any(u => u.Username == user.Username))
+            // {
+            //     return false; // Username already exists
+            // }
             // Add the new user to the list
-            users.Add(user);
+            // users.Add(user);
 
             // Save the modified user list to the file
-            SaveUserData();
+            // SaveUserData();
 
-
-            return true; // Registration successful
+            return foob.RegisterUser(user);
+            //return true; // Registration successful
         }
         
       /* public bool LoginUser(string username, string password)
@@ -65,240 +65,250 @@ namespace BusinessTierServer
 
         public User LoginUser(string username)
         {
-            User user = users.FirstOrDefault(u => u.Username == username);
-            if (user!=null)
-            { return user; }
-            return null;
+            //User user = users.FirstOrDefault(u => u.Username == username);
+            //if (user!=null)
+            //{ return user; }
+            //return null;
+            return foob.LoginUser(username);
         }
         public bool CreateGroupChat(ChatGroup chatGroup, User user)
         {
             // Check if the chat group name is unique
-            if (chatGroups.Any(c => c.Name == chatGroup.Name))
-            {
-                return false; // Chat group name already exists
-            }  
-               chatGroup.Members.Add(user);
-            // Update the JoinedGroups list for all users
-            foreach (User u in users)
-            {
-                if( u.Username.Equals( user.Username))
-                {
-                    u.JoinedGroups.Add(chatGroup); 
-                }
+            //if (chatGroups.Any(c => c.Name == chatGroup.Name))
+            //{
+            //    return false; // Chat group name already exists
+            //}  
+            //   chatGroup.Members.Add(user);
+            //// Update the JoinedGroups list for all users
+            //foreach (User u in users)
+            //{
+            //    if( u.Username.Equals( user.Username))
+            //    {
+            //        u.JoinedGroups.Add(chatGroup); 
+            //    }
 
-            }
-         
-            // Store the chat group in the list
-            chatGroups.Add(chatGroup);
-            // Save the modified chat groups list to the file
-            SaveChatGroups();
-            // Save the modified user data (including JoinedGroups) to the file
-            SaveUserData();
+            //}
 
-            return true; // Chat group creation successful
+            //// Store the chat group in the list
+            //chatGroups.Add(chatGroup);
+            //// Save the modified chat groups list to the file
+            //SaveChatGroups();
+            //// Save the modified user data (including JoinedGroups) to the file
+            //SaveUserData();
+
+            //return true; // Chat group creation successful
+            return foob.CreateGroupChat(chatGroup, user);
         }
 
         public bool JoinGroupChat(ChatGroup chatGroup, User user)
         {
+            return foob.JoinGroupChat(chatGroup, user);
             // Find the chat group with the given name
-            var targetChatGroup = chatGroups.FirstOrDefault(c => c.Name == chatGroup.Name);
+            //var targetChatGroup = chatGroups.FirstOrDefault(c => c.Name == chatGroup.Name);
 
-            if (targetChatGroup != null)
-            {
+            //if (targetChatGroup != null)
+            //{
 
-                // Check if the user is already a member of the group
-                if (!targetChatGroup.Members.Contains(user))
-                {
-                    targetChatGroup.Members.Add(user);
-                    // Update the JoinedGroups list for all users
-                
-                }
+            //    // Check if the user is already a member of the group
+            //    if (!targetChatGroup.Members.Contains(user))
+            //    {
+            //        targetChatGroup.Members.Add(user);
+            //        // Update the JoinedGroups list for all users
 
-                foreach (User u in users)
-                {
-                    if (u.Username.Equals(user.Username))
-                    {
-                        u.JoinedGroups.Add(targetChatGroup);
-                    }
+            //    }
 
-                }
+            //    foreach (User u in users)
+            //    {
+            //        if (u.Username.Equals(user.Username))
+            //        {
+            //            u.JoinedGroups.Add(targetChatGroup);
+            //        }
 
-
-                // Save the modified chat groups list to the file
-                SaveChatGroups();
-
-                // Save the modified user list to the file
-                SaveUserData();
-
-                return true;
-            }
+            //    }
 
 
+            //    // Save the modified chat groups list to the file
+            //    SaveChatGroups();
 
-            return false; // Chat group not found
+            //    // Save the modified user list to the file
+            //    SaveUserData();
+
+            //    return true;
+           // }
+
+
+
+            //return false; // Chat group not found
         }
 
         // Method to save chat groups to a JSON file
         public void SaveChatGroups()
         {
-            try
-            {
-                string filePath = "chatGroups.json";
+             foob.SaveChatGroups();
+            //try
+            //{
+            //    string filePath = "chatGroups.json";
 
-                // Serialize the list of chat groups to JSON format
-                string jsonData = JsonConvert.SerializeObject(chatGroups, Newtonsoft.Json.Formatting.Indented);
+            //    // Serialize the list of chat groups to JSON format
+            //    string jsonData = JsonConvert.SerializeObject(chatGroups, Newtonsoft.Json.Formatting.Indented);
 
-                // Write the JSON data to the file
-                File.WriteAllText(filePath, jsonData);
-            }
-            catch (Exception ex)
-            {
-                // Handle any exceptions that may occur during file saving
-                // You can log the error or take appropriate action
-            }
+            //    // Write the JSON data to the file
+            //    File.WriteAllText(filePath, jsonData);
+            //}
+            //catch (Exception ex)
+            //{
+            //    // Handle any exceptions that may occur during file saving
+            //    // You can log the error or take appropriate action
+            //}
         }
 
         // Method to load chat groups from a JSON file
         public List<ChatGroup> LoadChatGroups()
         {
-            try
-            {
-                string filePath = "chatGroups.json";
+            return foob.LoadChatGroups();
+            //try
+            //{
+            //    string filePath = "chatGroups.json";
 
-                // Check if the JSON file exists
-                if (File.Exists(filePath))
-                {
-                    // Read the JSON data from the file
-                    string jsonData = File.ReadAllText(filePath);
+            //    // Check if the JSON file exists
+            //    if (File.Exists(filePath))
+            //    {
+            //        // Read the JSON data from the file
+            //        string jsonData = File.ReadAllText(filePath);
 
-                    // Deserialize the JSON data back to a list of chat groups
-                    return JsonConvert.DeserializeObject<List<ChatGroup>>(jsonData);
-                }
-            }
-            catch (Exception ex)
-            {
-                // Handle any exceptions that may occur during file loading
-                // You can log the error or take appropriate action
-            }
+            //        // Deserialize the JSON data back to a list of chat groups
+            //        return JsonConvert.DeserializeObject<List<ChatGroup>>(jsonData);
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+            //    // Handle any exceptions that may occur during file loading
+            //    // You can log the error or take appropriate action
+            //}
 
-            return new List<ChatGroup>(); // Return an empty list if the file doesn't exist or there's an error
+            //return new List<ChatGroup>(); // Return an empty list if the file doesn't exist or there's an error
         }
 
         // Save user data to the JSON file
         public void SaveUserData()
         {
-            try
-            {
-                string filePath = "userList.json";
+               foob.SaveUserData();
+            //try
+            //{
+            //    string filePath = "userList.json";
 
-                // Serialize the list of users to JSON format
-                string jsonData = JsonConvert.SerializeObject(users, Newtonsoft.Json.Formatting.Indented);
+            //    // Serialize the list of users to JSON format
+            //    string jsonData = JsonConvert.SerializeObject(users, Newtonsoft.Json.Formatting.Indented);
 
-                // Write the JSON data to the file
-                File.WriteAllText(filePath, jsonData);
-            }
-            catch (Exception ex)
-            {
-                // Handle any exceptions that may occur during file saving
-                // You can log the error or take appropriate action
-            }
+            //    // Write the JSON data to the file
+            //    File.WriteAllText(filePath, jsonData);
+            //}
+            //catch (Exception ex)
+            //{
+            //    // Handle any exceptions that may occur during file saving
+            //    // You can log the error or take appropriate action
+            //}
         }
 
         // Load user data from the JSON file
         public List<User> LoadUserData()
         {
-            try
-            {
-                string filePath = "userList.json";
+            return foob.LoadUserData();
+            //try
+            //{
+            //    string filePath = "userList.json";
 
-                // Check if the JSON file exists
-                if (File.Exists(filePath))
-                {
-                    // Read the JSON data from the file
-                    string jsonData = File.ReadAllText(filePath);
+            //    // Check if the JSON file exists
+            //    if (File.Exists(filePath))
+            //    {
+            //        // Read the JSON data from the file
+            //        string jsonData = File.ReadAllText(filePath);
 
-                    // Deserialize the JSON data back to a list of users
-                    return JsonConvert.DeserializeObject<List<User>>(jsonData);
-                }
-            }
-            catch (Exception ex)
-            {
-                // Handle any exceptions that may occur during file loading
-                // You can log the error or take appropriate action
-            }
+            //        // Deserialize the JSON data back to a list of users
+            //        return JsonConvert.DeserializeObject<List<User>>(jsonData);
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+            //    // Handle any exceptions that may occur during file loading
+            //    // You can log the error or take appropriate action
+            //}
 
-            return new List<User>(); // Return an empty list if the file doesn't exist or there's an error
+            //return new List<User>(); // Return an empty list if the file doesn't exist or there's an error
         }
 
         public void handleMessage(ChatGroup chatGroup,ChatMessage newMessage)
         {
-            foreach (ChatGroup c in chatGroups)
-            {
-                if (c.Name.Equals(chatGroup.Name))
-                {
-                   c.ChatHistory.Add(newMessage);
-                }
+            //foreach (ChatGroup c in chatGroups)
+            //{
+            //    if (c.Name.Equals(chatGroup.Name))
+            //    {
+            //       c.ChatHistory.Add(newMessage);
+            //    }
 
-            }
+            //}
 
 
-            SaveChatGroups();
+            //SaveChatGroups();
+            foob.handleMessage(chatGroup, newMessage);
         }
 
         public List<ChatMessage> LoadChatHistory(ChatGroup chatGroup)
         {
-
+            return foob.LoadChatHistory(chatGroup);
             // Check if the chat group exists in your chatGroups list
-            var existingChatGroup = chatGroups.FirstOrDefault(c => c.Name == chatGroup.Name);
+            //var existingChatGroup = chatGroups.FirstOrDefault(c => c.Name == chatGroup.Name);
 
-            if (existingChatGroup != null)
-            {
-                // Return the chat history from the existing ChatGroup object
-                return existingChatGroup.ChatHistory;
-            }
-            else
-            {
-                // If the chat group doesn't exist, return an empty list
-                return new List<ChatMessage>();
-            }
+            //if (existingChatGroup != null)
+            //{
+            //    // Return the chat history from the existing ChatGroup object
+            //    return existingChatGroup.ChatHistory;
+            //}
+            //else
+            //{
+            //    // If the chat group doesn't exist, return an empty list
+            //    return new List<ChatMessage>();
+            //}
         }
 
         public void handleLeaveGroup(ChatGroup chatgroup, User user)
         {
-            foreach (ChatGroup c in chatGroups)
-            {
-                if (c.Name.Equals(chatgroup.Name))
-                {
-                    c.Members.Remove(user);
-                }
+            foob.handleLeaveGroup(chatgroup, user); 
+            //foreach (ChatGroup c in chatGroups)
+            //{
+            //    if (c.Name.Equals(chatgroup.Name))
+            //    {
+            //        c.Members.Remove(user);
+            //    }
 
-            }
+            //}
 
-            foreach (User u in users)
-            {
-                if (u.Username.Equals(user.Username))
-                {
-                    u.JoinedGroups.Remove(chatgroup);
-                }
+            //foreach (User u in users)
+            //{
+            //    if (u.Username.Equals(user.Username))
+            //    {
+            //        u.JoinedGroups.Remove(chatgroup);
+            //    }
 
-            }
+            //}
 
-            SaveChatGroups();
-            SaveUserData();
+            //SaveChatGroups();
+            //SaveUserData();
 
         }
 
         public List<User> LoadChatGroupMembers(ChatGroup chatgroup)
         {
-            foreach (ChatGroup c in chatGroups)
-            {
-                if (c.Name.Equals(chatgroup.Name))
-                {
-                    return c.Members;
-                }
+            return LoadChatGroupMembers(chatgroup);
+            //foreach (ChatGroup c in chatGroups)
+            //{
+            //    if (c.Name.Equals(chatgroup.Name))
+            //    {
+            //        return c.Members;
+            //    }
 
-            }
-            return new List<User>();
+            //}
+            //return new List<User>();
         }
 
      /*
--- ChatApplication/DatabaseServer/ServerInterface.cs
+++ ChatApplication/DatabaseServer/ServerInterface.cs
@@ -17,18 +17,47 @@ namespace DatabaseServer
         bool RegisterUser(User user);
 
         [OperationContract]
-        bool LoginUser(string username, string password);
+        // bool LoginUser(string username, string password);
+        User LoginUser(string username);
 
         [OperationContract]
-        bool CreateGroupChat(ChatGroup chatGroup);
+        // bool CreateGroupChat(ChatGroup chatGroup);
+        bool CreateGroupChat(ChatGroup chatGroup, User user);
+
+
 
         [OperationContract]
-        bool JoinGroupChat(string joinCode, string username);
+        void SaveChatGroups();
 
         [OperationContract]
-        List<ChatGroup> GetAllGroupChats();
+        List<ChatGroup> LoadChatGroups();
 
         [OperationContract]
-        List<string> GetUsersInChat(string chatName);
+        void handleMessage(ChatGroup chatGroup, ChatMessage chatMessage);
+
+        [OperationContract]
+        void handleLeaveGroup(ChatGroup chatGroup, User user);
+
+        [OperationContract]
+        List<ChatMessage> LoadChatHistory(ChatGroup chatGroup);
+
+        [OperationContract]
+        List<User> LoadChatGroupMembers(ChatGroup chatgroup);
+
+        [OperationContract]
+        // bool JoinGroupChat(string joinCode, string username);
+        bool JoinGroupChat(ChatGroup chatGroup, User user);
+
+        //   [OperationContract]
+        //   List<ChatGroup> GetAllGroupChats();
+
+        //   [OperationContract]
+        // List<string> GetUsersInChat(string chatName);
+
+        [OperationContract]
+        void SaveUserData();
+
+        [OperationContract]
+        List<User> LoadUserData();
     }
 }
